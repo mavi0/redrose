07bb2e8a0f329104dd9bf9fe3a718201efc24bd2
swartom <swartom@work.swarbrick.eu.org>
2024-11-25 15:48:50 +0000

    passthrough

--- .auctex-auto/Redrose.el
+++ .auctex-auto/Redrose.el
@@ -5,10 +5,10 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("color" "") ("tikz" "") ("transparent" "") ("biblatex" "backend=bibtex") ("graphicx" "") ("tikzpagenodes" "") ("background" "bottom" "top") ("atbegshi" "") ("geometry" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
    (TeX-run-style-hooks
     "colours"
     "commands/maketitle"
---
+++ .auctex-auto/exampleReport.el
@@ -0,0 +1,22 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "exampleReport"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("scrreprt" "")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("Redrose" "")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "scrreprt"
+    "scrreprt10"
+    "Redrose")
+   (LaTeX-add-bibliographies
+    "testing"))
+ :latex)
+
