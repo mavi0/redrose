00673539705006cc9031360483f53934ef04fa8d
swartom <swartom@work.swarbrick.eu.org>
2025-01-26 12:28:17 +0000

    Passthrough

--- .auctex-auto/exampleReport.el
+++ .auctex-auto/exampleReport.el
@@ -7,10 +7,10 @@
                      '(("scrreprt" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("Redrose" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "scrreprt"
